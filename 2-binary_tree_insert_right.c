d2f6f6db325396340afc2a1083fccc4eca5b1120
Mariam <eng.mariamnabil93@gmail.com>
2024-01-04 09:28:56 -0800

    1

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,23 +10,21 @@
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_n;
+	binary_tree_t *new;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	new_n = binary_tree_node(parent, value);
-	if (new_n == NULL)
-	{
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
-	}
+
 	if (parent->right != NULL)
 	{
-		new_n->right = parent->right;
-		parent->right->parent = new_n;
+		new->right = parent->right;
+		parent->right->parent = new;
 	}
-	parent->right = new_n;
-	return (new_n);
-}
+	parent->right = new;
+
+	return (new);
+{
